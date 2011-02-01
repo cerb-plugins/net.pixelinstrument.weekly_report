abc975d1940572b0bd14254f720efac64f3cb458
Davide Cassenti <davide.cassenti@gmail.com>
2011-02-01 02:34:02 +0100

    Solved bug in the sla code

--- api/toolbar/sla.php
+++ api/toolbar/sla.php
@@ -16,21 +16,15 @@ if (class_exists('Extension_TicketToolbarItem',true)):
 				false
 			);
 			
-			$now = time();
+			$response_date = $now;
 			
 			foreach ($tickets_messages as $message) {
 				$t_id = $message[SearchFields_Message::TICKET_ID];
 				
-				if (!isset($tickets[$t_id]['t_first_response_time']) || $tickets[$t_id]['t_first_response_time'] > $message[SearchFields_Message::CREATED_DATE]) {
-					$tickets[$t_id]['t_first_response_time'] = $message[SearchFields_Message::CREATED_DATE];
-				}
+				if ($response_date > $message[SearchFields_Message::CREATED_DATE])
+					$response_date = $message[SearchFields_Message::CREATED_DATE];
 			}
 			
-			if (isset ($ticket->t_first_response_time))
-                $response_date = $ticket->t_first_response_time;
-            else
-                $response_date = $now;
-            
             $tpl->assign ('ticket_sla', PiWeeklyReportPage::calculateWorkingDays ($ticket->created_date, $response_date));
 			
 			$tpl->display('devblocks:net.pixelinstrument.weekly_report::sla/toolbar.tpl');
