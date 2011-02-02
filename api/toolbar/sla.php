09afd33e1e24c912ff70c6c7301bbf61ab08e884
Davide Cassenti <davide.cassenti@gmail.com>
2011-02-02 23:41:13 +0100

    Added SLA, First Response and Last Response fields in the ticket page (replaced the old button that was showing only the SLA)

--- api/toolbar/sla.php
+++ api/toolbar/sla.php
@@ -4,6 +4,8 @@ if (class_exists('Extension_TicketToolbarItem',true)):
 		function render(Model_Ticket $ticket) {
 			$tpl = DevblocksPlatform::getTemplateService();
 			
+			$properties = PiWeeklyReportPage::_getProperties();
+			
 			list($tickets_messages) = DAO_Message::search(
 				array(
 					new DevblocksSearchCriteria(SearchFields_Message::TICKET_ID, DevblocksSearchCriteria::OPER_EQ, $ticket->id),
@@ -16,16 +18,51 @@ if (class_exists('Extension_TicketToolbarItem',true)):
 				false
 			);
 			
-			$response_date = $now;
+			$now = time();
+			
+			$first_response_date = $now;
+			$last_response_date = 0;
 			
 			foreach ($tickets_messages as $message) {
 				$t_id = $message[SearchFields_Message::TICKET_ID];
 				
-				if ($response_date > $message[SearchFields_Message::CREATED_DATE])
-					$response_date = $message[SearchFields_Message::CREATED_DATE];
+				if ($first_response_date > $message[SearchFields_Message::CREATED_DATE])
+					$first_response_date = $message[SearchFields_Message::CREATED_DATE];
+					
+				if ($last_response_date < $message[SearchFields_Message::CREATED_DATE]) {
+					$last_response_date = $message[SearchFields_Message::CREATED_DATE];
+				}
 			}
 			
-            $tpl->assign ('ticket_sla', PiWeeklyReportPage::calculateWorkingDays ($ticket->created_date, $response_date));
+			$sla = 0;
+			$customer_id = 0;
+			
+			// Get the customer id
+            $address = DAO_Address::get ($ticket->first_wrote_address_id);
+			if ($address)
+				$customer_id = $address->contact_org_id;
+				
+			if ($customer_id) {
+				$customer_custom_values = DAO_CustomFieldValue::getValuesByContextIds(CerberusContexts::CONTEXT_ORG, $customer_id);
+				
+				$customer_type_field_id = $properties['customer_type_field_id'];
+				$customer_type = "";
+				if ($customer_id &&
+					$customer_type_field_id &&
+					isset ($customer_custom_values[$customer_id]) &&
+					isset ($customer_custom_values[$customer_id][$customer_type_field_id]))
+				{
+					
+					$customer_type = $customer_custom_values[$customer_id][$customer_type_field_id];
+
+					$sla = isset ($properties['sla'][$customer_type]) ? ($properties['sla'][$customer_type]) : 0;
+				}
+			}
+			
+			$tpl->assign ('ticket_sla', $sla);
+			$tpl->assign ('customer_type', $customer_type);
+            $tpl->assign ('first_update', PiWeeklyReportPage::calculateWorkingDays ($ticket->created_date, $first_response_date));
+			$tpl->assign ('last_update', PiWeeklyReportPage::calculateWorkingDays ($last_response_date, $now));
 			
 			$tpl->display('devblocks:net.pixelinstrument.weekly_report::sla/toolbar.tpl');
 		}
--- templates/sla/toolbar.tpl
+++ templates/sla/toolbar.tpl
@@ -1,1 +1,6 @@
-<button type="button" id="sla_button"><span class="cerb-sprite sprite-stopwatch"></span> SLA: {$ticket_sla}d</button>
+<div style="padding: 4px; {if $ticket_sla > 0 && $first_update > $ticket_sla}color:#C00;{/if}">
+	<span class="cerb-sprite sprite-stopwatch" style="padding-bottom: 4px"></span>
+	<strong>SLA:</strong> {$ticket_sla} day{if $ticket_sla!=1}s {if $customer_type}({$customer_type}){/if}{/if}
+	| <strong>First Response:</strong> in {$first_update} day{if $first_update!=1}s{/if}
+	| <strong>Last Response:</strong> {$last_update} day{if $last_update!=1}s{/if} ago
+</div>
